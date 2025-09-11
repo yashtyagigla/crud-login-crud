e40ac0c027234809cbcee3010d988edaf192ddc1
Yash Tyagi <99109150+yashtyagigla@users.noreply.github.com>
2025-09-11 21:14:59 +0530

    publicPost

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -1,13 +1,16 @@
 const express  = require('express');
 const auth = require('../middleWare/auth');
-const {createPost,getMyPosts,getPostById,updatePost,deletePost} = require('../controllers/postController');
+const {createPost,getMyPosts,getPostById,updatePost,deletePost,getPublicPosts,getUserPublicPosts} = require('../controllers/postController');
 
 const router =express.Router();
+
 
 router.post('/',auth,createPost);
 router.get('/',auth, getMyPosts);
 router.get('/:id', auth, getPostById);
 router.put('/:id',auth,updatePost);
 router.delete('/:id',auth, deletePost);
+router.get("/post/public", getPublicPosts);
+router.get("/user/:id", getUserPublicPosts);
 
 module.exports = router;
